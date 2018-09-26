0dcd5f53c5389f20c580ddf729fb19f2c02b852b
Dara McHugh <dara.mchugh@gmail.com>
2018-09-26 22:36:30 +0100

    Spacing in wrong letters

--- src/Wrong.js
+++ src/Wrong.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 class WrongGuesses extends React.Component {
     render () {
+        let wrong = [...this.props.incorrect_guesses].map((letter) => {return letter + ' '});
         return (
-            <p className="lead" style={{textDecorationLine: 'line-through'}}>{ this.props.incorrect_guesses }</p>
+            <p className="lead" style={{textDecorationLine: 'line-through'}}>{ wrong }</p>
         );
     }
 }
