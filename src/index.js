6bd19e7169516d029d9fde7340e6c374de3cb5db
Dara McHugh <dara.mchugh@gmail.com>
2018-11-23 20:42:55 +0000

    Container for app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap';
 import './index.css';
 
 
